9fc7bd1844c9185f4bbeb26d85d5642f0839557b
abdelrahman adel <abdelrahman20191700364@cis.asu.edu.eg>
2024-02-06 10:38:52 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server :{
-    port : 5173
+  server: {
+    port: 5173,
   },
-  base: "/perfum-website-app/"
-})
+  base: "/perfum-website-app/",
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      include: [/perfum-website-app/, /node_modules/],
+    },
+  },
+});
